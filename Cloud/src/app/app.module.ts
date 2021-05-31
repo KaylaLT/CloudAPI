6bdf749268561c05070b9209db43c9ebb8d00f23
carmgrif <carmgrif@umich.edu>
2021-05-31 12:58:42 -0400

    pushing components/ need routing added

--- Cloud/src/app/app.module.ts
+++ Cloud/src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FavoritesComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -14,5 +33,6 @@ import { AppComponent } from './app.component';
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+});
+
 export class AppModule { }
