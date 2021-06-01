fc6f5f61f4ee2f7bc8b87b3c065b138b2123e551
carmgrif <carmgrif@umich.edu>
2021-06-01 01:09:31 -0400

    put statment

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -9,11 +9,6 @@ const db = pgp({
 
 
 
-
-
-
-
-
 routes.get('/apis', async (req, res) => {
     let sql = "select * from apis";
     if (req.query.category) {
@@ -21,6 +16,26 @@ routes.get('/apis', async (req, res) => {
     }
     const all = await db.manyOrNone('select * from apis where category = $(category)', {category: req.query.category});
     res.status(200).json(all);
+});
+
+
+routes.put('/apis', async (req, res) => {
+   
+    const update = await db.many('INSERT INTO apis (id, name, description, url, category, auth, cors) VALUES () RETURNING id', {
+        
+        id: req.body.id,
+        name: req.body.name,
+        description: req.body.description,
+        url: req.body.url,
+        category: req.body.category,
+        auth: req.body.auth,
+        cors: req.body.cors
+
+    });
+
+    const newAPI = await db.one('UPDATE id, name, description, url, category, auth, cors FROM apis WHERE id=${id}', { id: XPathResult.id }),
+        
+        return res.status(201).json(newAPI);
 });
 
 
