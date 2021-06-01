cb02e01a31232fc118777dc693a5c333f05bd233
steve erik <shenderson5454@gmail.com>
2021-06-01 08:28:17 -0400

    trying to get everything synced

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -34,14 +34,14 @@ routes.get('/apis', async (req, res) => {
 //           auth: req.body.auth,
 //           cors: req.body.cors
 //      }
-//     doors.push(newApis);
+//     apis.push(newApis);
 //      res.status(201).json(newApis);
 //  });
 
 
 routes.put('/apis', async (req, res) => {
    
-    const newAPI = await db.many('INSERT INTO apis (id, name, description, url, category, auth, cors) VALUES () RETURNING id', {
+    const update = await db.many('INSERT INTO apis (id, name, description, url, category, auth, cors) VALUES () RETURNING id', {
         
         id: req.body.id,
         name: req.body.name,
@@ -55,30 +55,10 @@ routes.put('/apis', async (req, res) => {
 
     const newAPI = await db.one('UPDATE id, name, description, url, category, auth, cors FROM apis WHERE id=${id}', { id: XPathResult.id }),
         
-        return res.status(201).json(newAPI);
+        return res.status(201).json(update);
 });
 
 
-routes.put('/apis', async (req, res) => {
-
-    const newAPI = await db.many('INSERT INTO apis (id, name, description, url, category, auth, cors) VALUES () RETURNING id', {
-
-        id: req.body.id,
-        name: req.body.name,
-        description: req.body.description,
-        url: req.body.url,
-        category: req.body.category,
-        auth: req.body.auth,
-        cors: req.body.cors
-
-    });
-
-    const newAPI = await db.one('UPDATE id, name, description, url, category, auth, cors FROM apis WHERE id=${id}', {
-            id: XPathResult.id
-        }),
-
-        return res.status(201).json(newAPI);
-});
 
 
 
