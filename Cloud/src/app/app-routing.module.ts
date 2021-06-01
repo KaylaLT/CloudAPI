14ef611111dc4a793b8564b1798fa6e43b710c4b
carmgrif <carmgrif@umich.edu>
2021-06-01 00:43:53 -0400

    fixed routing & npm installed

--- Cloud/src/app/app-routing.module.ts
+++ Cloud/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Cloud/src/app/app.module.ts
+++ Cloud/src/app/app.module.ts
@@ -1,31 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-
-
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'favorites', component: FavoritesComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
-];
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    FavoritesComponent,
-    LoginComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -33,6 +15,6 @@ const routes: Routes = [
   ],
   providers: [],
   bootstrap: [AppComponent]
-});
+})
 
 export class AppModule { }
