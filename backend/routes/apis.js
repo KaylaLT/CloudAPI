4c5e21cc158a8b7c6b62a1d2c68046c18fc64d39
steve erik <shenderson5454@gmail.com>
2021-06-03 16:08:34 -0400

    got post to work / now have 4 of the 5 endpoints working

--- backend/routes/apis.js
+++ backend/routes/apis.js
@@ -16,6 +16,7 @@ routes.get('/apis', async (req, res) => {
         const all = await db.manyOrNone('select * from apis')
         res.status(200).json(all);
     }
+    
 });
 
 routes.get('/apis/:category', async (req, res) => {
@@ -23,15 +24,18 @@ routes.get('/apis/:category', async (req, res) => {
     res.json(await db.oneOrNone('SELECT * FROM apis WHERE id = $(category)', {
         category: req.params.category
     }));
+    console.log('working 2')
     if (!category) {
         return res.status(404).send('Category could not be found')
     }
     res.json(category)
+    
 });
+
 
 routes.put('/apis', async (req, res) => {
     try {
-        const apiUpdated = await db.one('INSERT INTO apis (id, name, description, url, category, auth, cors) VALUES ( $(id), $(name), $(description), $(url), $(category), $(auth), $(cors)) RETURNING id', {
+        const apiUpdated = await db.oneOrNone('INSERT INTO apis (id, name, description, url, category, auth, cors) VALUES ( ${id}, ${name}, ${description}, ${url}, ${category}, ${auth}, ${cors}) RETURNING id', {
             id: req.body.id,
             name: req.body.name,
             description: req.body.description,
@@ -40,13 +44,16 @@ routes.put('/apis', async (req, res) => {
             auth: req.body.auth,
             cors: req.body.cors
         });
+        console.log('working 3')
         res.status(201).json(apiUpdated);
     } catch (error) {
-        if (error.constraint === 'url') {
-            return res.status(500).send('this url already has a home')
+        if (error.constraint === 'apis_pkey') {
+            return res.status(500).send('your update has failed')
         }
     }
+    
 });
+
 
 routes.post('/apis', async (req, res) => {
     try {
@@ -54,28 +61,32 @@ routes.post('/apis', async (req, res) => {
             name: req.body.name,
             description: req.body.description,
             url: req.body.url,
-            category: req.body.category,
+            category: req.body.category, 
             auth: req.body.auth,
             cors: req.body.cors,
         });
-        console.log('working')
+        console.log('working 4')
         
-        const posted = await db.one('SELECT id, name, description, url, category, auth, cors FROM apis WHERE id = ${id}', { id: newApi.id });
+        const posted = await db.oneOrNone('SELECT id, name, description, url, category, auth, cors FROM apis WHERE id = ${id}', { id: newApi.id });
         return res.status(201).json(posted)
     } catch (error) {
         if (error.constraint === 'url') {
             return res.status(400).json(error);
         }
     }
+    console.log('working 5')
 
 });
+
 
 
     routes.delete('/apis/:id', async (req, res) => {
         await db.none('DELETE FROM apis WHERE id =${id}', {
             id: +req.params.id
         });
+        console.log('working 6');
         return res.status(204).send();
+        
     });
 
 
