1883ae9d83ab383888b58f80feb0f42d2468a978
steve erik <shenderson5454@gmail.com>
2021-06-02 20:07:09 -0400

    still trying

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const routes = require('./routes/api');
+const routes = require('./routes/apis');
 const port = 3000
 const app = express();
 app.use(express.json());
 app.use('/', routes);
-
 app.listen(port, () => console.log(`Running the best code on port: ${port}.`));
