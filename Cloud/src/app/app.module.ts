4af8d6446b3cfb1db31cf34d380f3f9f1ef9a695
carmgrif <carmgrif@umich.edu>
2021-06-07 21:46:51 -0400

    injected api service in app ts file

--- Cloud/src/app/app.module.ts
+++ Cloud/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ApiService
   ],
   providers: [],
   bootstrap: [AppComponent]
