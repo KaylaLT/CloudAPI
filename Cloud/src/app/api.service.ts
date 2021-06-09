0f7684fa7676c7cd482c33180f2ba017f7b93719
carmgrif <carmgrif@umich.edu>
2021-06-09 00:22:57 -0400

    small edits on service functions

--- Cloud/src/app/api.service.ts
+++ Cloud/src/app/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { stringify } from '@angular/compiler/src/util';
 
 
 @Injectable({
@@ -9,7 +10,8 @@ import { map } from 'rxjs/operators';
 })
 export class ApiService {
 
-  apiUrl: string = 'http://localhost:3000/apis/:id';
+  apiUrl: string = 'http://localhost:3000/apis';
+  idUrl: string = 'http://localhost:3000/apis/:id';
   categoryUrl: string = 'http://localhost:3000/apis/?=category';
   updateUrl: string = 'http://localhost:3000/apis/:id';
   postUrl: string = 'http://localhost:3000/apis';
@@ -19,29 +21,40 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
 
+  searchAPIS(search: any | null) {
+    let searchDatabase = {
+      
+    }
+      
+  }
+
 
 
   getID(id: number){
-    return this.http.get(this.apiUrl);
+    return this.http.get(this.idUrl);
     // `http://localhost:3000/apis/${id}`
   };
 
-  getCategory(){
+
+  getCategory(category: string){
     return this.http.get(this.categoryUrl);
     // 'http://localhost:3000/apis/?=category'
   };
+
 
   postNewAPI(){
     return this.http.get(this.postUrl);
     // 'http://localhost:3000/apis'
   };
 
-  update(id: any){
+
+  update(id: number){
     return this.http.get(this.updateUrl);
     // `http://localhost:3000/apis/${id}`
   };
 
-  delete(id: any){
+
+  delete(id: number){
     return this.http.delete(this.deleteUrl);
     // `http://localhost:3000/apis/${id}`
   }
